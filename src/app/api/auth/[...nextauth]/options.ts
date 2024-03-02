f7cac7706d4c4e73d75c851c9c6456a82ad1761b
cody proo <cody20v@gmail.com>
2024-03-02 15:17:32 +0330

    X

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,45 +1,45 @@
-import { ILoginRequestBody } from "@/types/auth.type";
-import { loginMutationApi } from "@/services/auth";
-import { AuthOptions } from "next-auth";
-import NextAuthCredentials from "next-auth/providers/credentials";
+import { ILoginRequestBody } from '@/types/auth.type';
+import { loginMutationApi } from '@/services/auth';
+import { AuthOptions } from 'next-auth';
+import NextAuthCredentials from 'next-auth/providers/credentials';
 
 export const authOptions: AuthOptions = {
-	secret: "xxx",
-	session: {
-		strategy: "jwt",
-	},
-	providers: [
-		NextAuthCredentials({
-			type: "credentials",
-			credentials: {
-				phone: { type: "string" },
-				password: { type: "string" },
-				type: { type: "string" },
-			},
-			async authorize(data?: ILoginRequestBody) {
-				const response = await loginMutationApi(data as ILoginRequestBody);
-				return response?.token
-					? {
-							...response,
-							accessToken: response.token,
-							id: response?.user?.id! as any,
-					  }
-					: null;
-			},
-		}),
-	],
-	callbacks: {
-		jwt: ({ user, token, session }: any) => {
-			if (session) token.user = { ...session };
-			if (user) return { ...(user || {}), ...(token || {}) };
-			return { ...token };
-		},
-		async session({ session, token }: any) {
-			if (token?.user) {
-				session.user = token.user;
-				session.accessToken = token.accessToken;
-			}
-			return session;
-		},
-	},
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: 'jwt',
+  },
+  providers: [
+    NextAuthCredentials({
+      type: 'credentials',
+      credentials: {
+        phone: { type: 'string' },
+        password: { type: 'string' },
+        type: { type: 'string' },
+      },
+      async authorize(data?: ILoginRequestBody) {
+        const response = await loginMutationApi(data as ILoginRequestBody);
+        return response?.token
+          ? {
+              ...response,
+              accessToken: response.token,
+              id: response?.user?.id! as any,
+            }
+          : null;
+      },
+    }),
+  ],
+  callbacks: {
+    jwt: ({ user, token, session }: any) => {
+      if (session) token.user = { ...session };
+      if (user) return { ...(user || {}), ...(token || {}) };
+      return { ...token };
+    },
+    async session({ session, token }: any) {
+      if (token?.user) {
+        session.user = token.user;
+        session.accessToken = token.accessToken;
+      }
+      return session;
+    },
+  },
 };
