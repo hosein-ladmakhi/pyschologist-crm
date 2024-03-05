8ae5eb8733d5150c161246c2fdde73debeb514f8
cody proo <cody20v@gmail.com>
2024-03-05 16:45:17 +0330

    middleware

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,9 +9,6 @@ import ToastifyProvider from "@/providers/ToastifyProvider";
 import SessionProvider from "@/providers/SessionProvider";
 import { getServerSession } from "next-auth";
 import { Metadata } from "next";
-import { userAgent } from "next/server";
-import { headers } from "next/headers";
-import { redirect, usePathname } from "next/navigation";
 
 export const metadata: Metadata = {
   manifest: "/manifest.json",
@@ -19,8 +16,6 @@ export const metadata: Metadata = {
 
 const RootLayout: FC<PropsWithChildren> = async ({ children }) => {
   const session = await getServerSession();
-  const isMobilePlatform = userAgent({ headers: headers() }).device.type === "mobile";
-  const desktopPageURL = "/main-operation";
   return (
     <html lang="fa" dir="rtl">
       <body className={iranYekanFont.className}>
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,31 @@
-export { default } from 'next-auth/middleware';
+import { headers } from "next/headers";
+import { NextFetchEvent, NextRequest, NextResponse, userAgent } from "next/server";
+import { withAuth } from "next-auth/middleware";
 
-export const config = { matcher: ['/tickets', '/dashboard'] };
+const protectedRoute = ["/dashboard", "/tickets"];
+
+const protectedMiddleware = withAuth({});
+
+const withPlatform = (handler) => {
+  return (request: NextRequest) => {
+    const isMobilePlatform = userAgent({ headers: headers() }).device.type === "mobile";
+    const desktopPageURL = "/main-operation";
+    const currentURL = request.nextUrl.pathname;
+    if (isMobilePlatform && currentURL !== desktopPageURL) {
+      return NextResponse.redirect(request.nextUrl.origin + desktopPageURL);
+    }
+    return handler(request);
+  };
+};
+
+const withNextAuth = () => {
+  return (request: NextRequest, ev: NextFetchEvent) => {
+    const currentURL = request.nextUrl.pathname;
+    if (protectedRoute.includes(currentURL)) {
+      return protectedMiddleware(request as any, ev);
+    }
+    return NextResponse.next();
+  };
+};
+
+export default withPlatform(withNextAuth());
